e8ddcd4b3639c5176becde99d8437a2cee1c2b5a
NamPE286 <nambuihung654@gmail.com>
2025-01-19 22:42:29 +0700

    doc

--- api/routers/user.ts
+++ api/routers/user.ts
@@ -1,9 +1,23 @@
+/**
+ * Express router providing user related routes.
+ * @module routers/user
+ */
+
 // @ts-types="@types/express"
 import express from "express";
 import { supabase } from "#api/supabase.ts";
 
 const router = express.Router();
 
+/**
+ * PATCH / route to update user information.
+ * @name PATCH/
+ * @function
+ * @memberof module:routers/user
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 router.patch("/", async (req, res) => {
     if (req.body.user_id !== res.locals.user.user_id) {
         res.status(403).send();
@@ -22,4 +36,9 @@ router.patch("/", async (req, res) => {
     res.send();
 });
 
+/**
+ * Express router to mount user related functions on.
+ * @type {Object}
+ * @memberof module:routers/user
+ */
 export default router;
