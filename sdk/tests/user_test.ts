ef3327772c800f79c86f123efa57f8d42f23fdc8
NamPE286 <nambuihung654@gmail.com>
2025-01-20 18:10:05 +0700

    Refactor user tests for improved readability and consistency

--- sdk/tests/user_test.ts
+++ sdk/tests/user_test.ts
@@ -3,41 +3,35 @@ import { Client } from "#sdk/mod.ts";
 import signedInClient from "#sdk/tests/utils/signedInClient.ts";
 import "jsr:@std/dotenv/load";
 
-Deno.test({
-    name: "Edit user by UID",
-    fn: async () => {
-        const client = await signedInClient();
-        const { data } = await client.db.auth.getUser();
+Deno.test("Edit user by UID", async () => {
+    const client = await signedInClient();
+    const { data } = await client.db.auth.getUser();
 
-        await client.user.update({
-            user_id: data.user?.id,
-            name: "test123",
-        });
+    await client.user.update({
+        user_id: data.user?.id,
+        name: "test123",
+    });
 
-        const user = await client.user.get(data.user?.id!);
+    const user = await client.user.get(data.user?.id!);
 
-        client.db.auth.stopAutoRefresh();
+    client.db.auth.stopAutoRefresh();
 
-        assertEquals(user.name, "test123");
-    },
+    assertEquals(user.name, "test123");
 });
 
-Deno.test({
-    name: "Get user by UID",
-    fn: async () => {
-        const client = new Client(Deno.env.get("SUPABASE_API_URL")!, Deno.env.get("SUPABASE_ANON_KEY")!, Deno.env.get("API_URL")!);
-        const user = await client.user.get("ded6b269-a856-4a49-a1ae-d8837d50e350");
+Deno.test("Get user by UID", async () => {
+    const client = new Client(Deno.env.get("SUPABASE_API_URL")!, Deno.env.get("SUPABASE_ANON_KEY")!, Deno.env.get("API_URL")!);
+    const user = await client.user.get("ded6b269-a856-4a49-a1ae-d8837d50e350");
 
-        client.db.auth.stopAutoRefresh();
+    client.db.auth.stopAutoRefresh();
 
-        assertEquals(user, {
-            user_id: "ded6b269-a856-4a49-a1ae-d8837d50e350",
-            created_at: "2025-01-18T09:56:19.965229+00:00",
-            name: "default",
-            city: "Hạ Long",
-            province: "Quảng Ninh",
-            role: "default",
-            is_hidden: false,
-        });
-    },
+    assertEquals(user, {
+        user_id: "ded6b269-a856-4a49-a1ae-d8837d50e350",
+        created_at: "2025-01-18T09:56:19.965229+00:00",
+        name: "default",
+        city: "Hạ Long",
+        province: "Quảng Ninh",
+        role: "default",
+        is_hidden: false,
+    });
 });
