889e84c57fd6daff88f6c091117d9a004c547ee2
NamPE286 <nambuihung654@gmail.com>
2025-01-22 08:07:22 +0700

    Refactor Level class: enhance data structure and add rating retrieval method; update tests for new functionality

--- sdk/classes/level.ts
+++ sdk/classes/level.ts
@@ -1,16 +1,35 @@
 import type { SupabaseClient } from "@supabase/supabase-js";
-import type { Database } from "#types/supabase.ts";
+import type { Database, Tables } from "#types/supabase.ts";
 
 type TLevel = Database["public"]["Tables"]["levels"];
+type TLevelData = TLevel["Row"] & { ratings: Tables<"level_rating">[] };
+
+export interface LevelData extends TLevelData {}
+
+export class LevelData {
+    getRating(list: string) {
+        for (const i of this.ratings) {
+            if (i.list === list) {
+                return i;
+            }
+        }
+
+        return null;
+    }
+
+    constructor(data: TLevelData) {
+        Object.assign(this, data);
+    }
+}
 
 export class Level {
     private db: SupabaseClient<Database>;
     private APIUrl: string;
 
-    async get(id: number) {
+    async get(id: number): Promise<LevelData> {
         const { data, error } = await this.db
             .from("levels")
-            .select("*, level_rating(*)")
+            .select("*, ratings:level_rating(*)")
             .eq("id", id)
             .single();
 
@@ -18,7 +37,7 @@ export class Level {
             throw error;
         }
 
-        return data;
+        return new LevelData(data);
     }
 
     constructor(_db: SupabaseClient<Database>, _APIUrl: string) {
--- tests/level_test.ts
+++ tests/level_test.ts
@@ -1,5 +1,29 @@
-import { createClient } from "#sdk/utils/client.ts";
+import { client } from "#sdk/utils/client.ts";
+import { assertEquals } from "@std/assert/equals";
 
 Deno.test("Get level by ID", async () => {
+    const level = await client.level.get(52374843);
 
+    assertEquals(JSON.parse(JSON.stringify(level)), {
+        id: 52374843,
+        created_at: "2025-01-19T18:16:01.572288+00:00",
+        name: "Zodiac",
+        creator: "Bianox and more",
+        youtube_video_id: "FX9paD5rRsM",
+        ratings: [
+            { id: 52374843, list: "demon", rating: 3000, min_progress: 60 },
+            { id: 52374843, list: "featured", rating: 1000, min_progress: 100 },
+        ],
+    });
+});
+
+Deno.test("Get level rating", async () => {
+    const res = (await client.level.get(52374843)).getRating("demon");
+
+    assertEquals(JSON.parse(JSON.stringify(res)), {
+        id: 52374843,
+        list: "demon",
+        rating: 3000,
+        min_progress: 60,
+    });
 });
