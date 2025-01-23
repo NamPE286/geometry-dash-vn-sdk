11de547a3213123d021bd945327a5d5c8a8950a0
NamPE286 <nambuihung654@gmail.com>
2025-01-23 20:48:18 +0700

    refactor: integrate setupTest utility in list test for improved structure and clarity

--- tests/list_test.ts
+++ tests/list_test.ts
@@ -1,37 +1,42 @@
 import { LevelData } from "#src/classes/level.ts";
 import { assertEquals } from "@std/assert/equals";
-import { client } from "./utils/client.ts";
+import { setupTest } from "./utils/environment.ts";
+import type { Client } from "#src/mod.ts";
 
 Deno.test("Get list", async () => {
-    const list = await client.list.getLevels("demon", { start: 0, end: 1 });
-    const cmp: LevelData[] = [
-        new LevelData({
-            id: 71025973,
-            name: "Oblivion",
-            creator: "dice88 & more",
-            ratings: [
-                { id: 71025973, list: "demon", rating: 4100, min_progress: 60 },
-            ],
-            created_at: "2025-01-18T10:25:33.965152+00:00",
-            youtube_video_id: "bsWqS5QPhz8",
-        }),
-        new LevelData({
-            id: 52374843,
-            name: "Zodiac",
-            creator: "Bianox and more",
-            ratings: [
-                { id: 52374843, list: "demon", rating: 3000, min_progress: 60 },
-                {
+    await setupTest({
+        fn: async (client: Client) => {
+            const list = await client.list.getLevels("demon", { start: 0, end: 1 });
+            const cmp: LevelData[] = [
+                new LevelData({
+                    id: 71025973,
+                    name: "Oblivion",
+                    creator: "dice88 & more",
+                    ratings: [
+                        { id: 71025973, list: "demon", rating: 4100, min_progress: 60 },
+                    ],
+                    created_at: "2025-01-18T10:25:33.965152+00:00",
+                    youtube_video_id: "bsWqS5QPhz8",
+                }),
+                new LevelData({
                     id: 52374843,
-                    list: "featured",
-                    rating: 1000,
-                    min_progress: 100,
-                },
-            ],
-            created_at: "2025-01-19T18:16:01.572288+00:00",
-            youtube_video_id: "FX9paD5rRsM",
-        }),
-    ];
+                    name: "Zodiac",
+                    creator: "Bianox and more",
+                    ratings: [
+                        { id: 52374843, list: "demon", rating: 3000, min_progress: 60 },
+                        {
+                            id: 52374843,
+                            list: "featured",
+                            rating: 1000,
+                            min_progress: 100,
+                        },
+                    ],
+                    created_at: "2025-01-19T18:16:01.572288+00:00",
+                    youtube_video_id: "FX9paD5rRsM",
+                }),
+            ];
 
-    assertEquals(list, cmp);
+            assertEquals(list, cmp);
+        },
+    });
 });
