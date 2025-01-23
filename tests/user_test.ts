61318a5daa5b5a93b329b07a10c89911b1575506
NamPE286 <nambuihung654@gmail.com>
2025-01-23 20:29:59 +0700

    refactor: enhance user test setup and improve error handling

--- tests/user_test.ts
+++ tests/user_test.ts
@@ -1,46 +1,50 @@
 import { assertEquals } from "jsr:@std/assert";
-import { client, signInClient, signOutClient } from "./utils/client.ts";
-import { setup } from "./utils/environment.ts";
+import { setup, signOutClient } from "./utils/environment.ts";
 import "jsr:@std/dotenv/load";
-
+import type { Client } from "#src/mod.ts";
 Deno.test("Insert new user", async () => {
-    try {
-        const { data, error } = await client.db.auth.signUp({
-            email: "test@bitbucket.local",
-            password: "123456",
-        });
+    await setup({
+        signedIn: false,
+        fn: async (client: Client) => {
+            try {
+                const { data, error } = await client.db.auth.signUp({
+                    email: "test@bitbucket.local",
+                    password: "123456",
+                });
 
-        if (error) {
-            throw error;
-        }
+                if (error) {
+                    throw error;
+                }
 
-        await client.user.create({ name: "newuser" });
+                await client.user.create({ name: "newuser" });
 
-        const { data: user } = await client.user.get(data.user?.id!);
-        user.created_at = "";
+                const { data: user } = await client.user.get(data.user?.id!);
+                user.created_at = "";
 
-        assertEquals(user, {
-            user_id: data.user?.id!,
-            created_at: "",
-            name: "newuser",
-            city: null,
-            province: null,
-            role: "default",
-            is_hidden: false,
-        });
-    } catch (err) {
-        await signOutClient();
+                assertEquals(user, {
+                    user_id: data.user?.id!,
+                    created_at: "",
+                    name: "newuser",
+                    city: null,
+                    province: null,
+                    role: "default",
+                    is_hidden: false,
+                });
+            } catch (err) {
+                await signOutClient();
 
-        throw err;
-    }
+                throw err;
+            }
 
-    await signOutClient();
+            await signOutClient();
+        },
+    });
 });
 
 Deno.test("Edit user by UID", async () => {
     await setup({
         signedIn: true,
-        fn: async () => {
+        fn: async (client: Client) => {
             const { data } = await client.db.auth.getUser();
 
             await client.user.update({
@@ -58,16 +62,21 @@ Deno.test("Edit user by UID", async () => {
 Deno.test(
     "Get user by UID",
     async () => {
-        const { data } = await client.user.get("ded6b269-a856-4a49-a1ae-d8837d50e350");
+        await setup({
+            signedIn: false,
+            fn: async (client: Client) => {
+                const { data } = await client.user.get("ded6b269-a856-4a49-a1ae-d8837d50e350");
 
-        assertEquals(data, {
-            user_id: "ded6b269-a856-4a49-a1ae-d8837d50e350",
-            created_at: "2025-01-18T09:56:19.965229+00:00",
-            name: "default",
-            city: "Hạ Long",
-            province: "Quảng Ninh",
-            role: "default",
-            is_hidden: false,
+                assertEquals(data, {
+                    user_id: "ded6b269-a856-4a49-a1ae-d8837d50e350",
+                    created_at: "2025-01-18T09:56:19.965229+00:00",
+                    name: "default",
+                    city: "Hạ Long",
+                    province: "Quảng Ninh",
+                    role: "default",
+                    is_hidden: false,
+                });
+            },
         });
     },
 );
--- tests/utils/environment.ts
+++ tests/utils/environment.ts
@@ -1,14 +1,58 @@
-import { signInClient, signOutClient } from "./client.ts";
+import { Client } from "#src/mod.ts";
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "#src/types/supabase.ts";
+import "jsr:@std/dotenv/load";
+
+export const client = new Client(
+    Deno.env.get("SUPABASE_API_URL")!,
+    Deno.env.get("SUPABASE_ANON_KEY")!,
+    Deno.env.get("API_URL")!,
+);
+
+export const server = createClient<Database>(
+    Deno.env.get("SUPABASE_API_URL")!,
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
+);
+
+export async function signInClient(role: string = "default") {
+    await server.auth.admin.createUser({
+        email: "test@bitbucket.local",
+        password: "123456",
+        email_confirm: true,
+    });
+
+    const { data } = await client.db.auth.signInWithPassword({
+        email: "test@bitbucket.local",
+        password: "123456",
+    });
+
+    await server
+        .from("users")
+        .insert({ user_id: data.user?.id!, name: "test", role: role });
+
+    return client;
+}
+
+export async function signOutClient() {
+    const { data } = await client.db.auth.getUser();
+
+    await server
+        .from("users")
+        .delete()
+        .eq("user_id", data.user!.id);
+
+    await server.auth.admin.deleteUser(data.user!.id);
+}
 
 export async function setup(
-    { signedIn = false, fn }: { signedIn: boolean; fn: () => Promise<void> },
+    { signedIn = false, fn }: { signedIn?: boolean; fn: (client: Client) => Promise<void> },
 ) {
     if (signedIn) {
         await signInClient();
     }
 
     try {
-        await fn();
+        await fn(client);
     } catch (err) {
         if (signedIn) {
             await signOutClient();
